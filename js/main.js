153ce8f359b19dc7e00711895583825e23fed9f5
Claudio Vilas <claudiogvilas@gmail.com>
2022-09-12 10:49:39 -0300

    version2

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//PROMPT
+//prompt ("Por favor ingresa tu nombre")
+//let nombreUsuario = prompt ("Cual es tu nombre");
+
+//console.log("El nombre ingresado es" + " " + nombreUsuario);
+
+//alert(`Cuidado estas por entrar a un sitio imprevisible ${nombreUsuario}`);
+
+
+let nombre = prompt("Cual es tu nombre?");
+let apellido = prompt("Tambien decinos cua es tu apellido");
+let edad = prompt("Por favor decinos tu edad");
+const espacio = " ";
+
+alert(`Cuidado ${nombre} ${apellido} de ${edad} anios, ya me diste todos tus datos`);
