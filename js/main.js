f7128ca3abfa2e653c1767f71bbd58362d23eb50
Claudio Vilas <claudiogvilas@gmail.com>
2022-09-14 12:33:18 -0300

    version2

--- js/main.js
+++ js/main.js
@@ -7,9 +7,28 @@
 //alert(`Cuidado estas por entrar a un sitio imprevisible ${nombreUsuario}`);
 
 
-let nombre = prompt("Cual es tu nombre?");
-let apellido = prompt("Tambien decinos cua es tu apellido");
-let edad = prompt("Por favor decinos tu edad");
-const espacio = " ";
+//let nombre = prompt("Cual es tu nombre?");
+//let apellido = prompt("Tambien decinos cua es tu apellido");
+//let edad = prompt("Por favor decinos tu edad");
+//const espacio = " ";
 
-alert(`Cuidado ${nombre} ${apellido} de ${edad} anios, ya me diste todos tus datos`);
+//333alert(`Cuidado ${nombre} ${apellido} de ${edad} anios, ya me diste todos tus datos`);
+
+// let precio = parseFloat(prompt("Cual es el precio"));
+
+// if(precio > 100) {
+//     console.log("Que caro!");
+// }
+// else if (precio == 30) {
+//     console.log("Que buenos, justo lo que tengo en el bolsillo");
+// }
+// else{
+//     console.log("Lo llevo");
+//}
+
+// let num1 = 1
+// let numUno = "1"
+
+// let sonIguales = (num1 === numUno)
+
+// console.log(sonIguales)
