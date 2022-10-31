9045b44d2c347673e88281353f02e1f6e5ac8cb6
Claudio Vilas <claudiogvilas@gmail.com>
2022-10-30 21:53:25 -0300

    array

--- js/conocimientodemasde5anos.js
+++ js/conocimientodemasde5anos.js
@@ -3,10 +3,28 @@ const datoDelaApi = document.getElementById("datoDelaApi");
 
 
 const obtenerDatos = () => {
+    fetch ("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+    ) 
+    .then(response => response.json())
+    .then (result =>{        console.log(result)
+        let data = result.Symbol;
+        console.log(data)
+    } )}
 
-}
+    
+
 
 
 botonCunsulta.onclick = () => {
-    obtenerDatos()
+    obtenerDatos();
 }
+
+
+let ws = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
+let stockPriceElement = document.getElementById("stock-price")
+
+
+ws.onmessage = (event) => {
+    let stockObject = JSON.parse(event.data);
+    stockPriceElement.innerHTML = parseFloat(stockObject.p).toFixed(2);
+};
