17963b42867ecb80c1eca17eaefc7f09b7eb10c1
Claudio Vilas <claudiogvilas@gmail.com>
2022-11-06 11:37:32 -0300

    mail

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 
 
-let boton = document.getElementById("boton");
+let boton = document.getElementById("my-form-button");
 let mailUsuario = document.querySelector("#mailUsuario")
 
 boton.addEventListener ("click", (e) =>{
@@ -13,10 +13,9 @@ boton.addEventListener ("click", (e) =>{
 
         localStorage.setItem("edadUsuario", edadUsuario.value);
 
+        if (localStorage.getItem ("mailUsuario") != document.querySelector("#mailUsuario").value){
+
         localStorage.setItem("mailUsuario", mailUsuario.value);
-
-        if (localStorage.getItem ("mailUsuario") != document.querySelector("#mailUsuario").value) { 
-
 
         Swal.fire('Tu datos han sido enviados correctamente').then (() =>  {
         
@@ -32,8 +31,10 @@ boton.addEventListener ("click", (e) =>{
 }
 
 else{
-        Swal.fire('Tu datos ya han sido ingresado te enviaremos al carrito de compras')
+        e.preventDefault()
+        Swal.fire('Tu datos ya han sido ingresado te enviaremos al carrito de compras').then(() => {
         {document.location.href ="../pages/conocimientode3a5anos.html"}
+        })
         ;
 }
      
@@ -41,6 +42,37 @@ else{
 );
 
 
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit);
 
 
 
